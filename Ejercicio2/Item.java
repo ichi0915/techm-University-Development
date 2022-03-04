02174114c601e301cbe87833d5e25de121a8f559
AndreaLopez <andrealopez@TechMs-MacBook-Pro.local>
2022-03-04 11:34:11 -0600

    Ejercicio 2

---
+++ Ejercicio2/Item.java
@@ -0,0 +1,14 @@
+package Ejercicio2;
+public interface Item {
+    //public String name;
+    //public int amount;
+    //public int amountLimit;
+    //public int UUID_id;
+
+    public void setName(String name);
+    public String getName();
+    public void setAmount(int amount);
+    public int getAmount();
+    public void setAmountLimit(int amountLimit);
+    public int getAmountLimit();
+}
